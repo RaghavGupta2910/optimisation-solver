e9b53002945f918d71da7d87e4fb4eecd3bf5868
RaghavGupta2910 <raghavgupta291024@gmail.com>
2026-08-26 14:49:55 +0530

    Add solver and instance CLI arguments

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,16 @@
 #include <iostream>
+#include <string>
+using namespace std;
 
-int main() {
-  std::cout << "Optimization Solver Benchmark Harness\n";
+int main(int argc, char *argv[]) {
+
+  if (argc == 5 && std::string(argv[1]) == "--solver" &&
+      std::string(argv[3]) == "--instances") {
+    string solverPath = argv[2];
+    string instanceFile = argv[4];
+    cout << "Solver: " << solverPath << '\n';
+    cout << "Instances: " << instanceFile << '\n';
+  }
+
   return 0;
 }
